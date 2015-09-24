810d2a70544816d94eb454bf070fe44949aedb67
liufffan <liufan@gmail.com>
2015-09-24 19:52:03 +0800

    Update docker-entrypoint.sh

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -23,6 +23,7 @@ HACKPAD_SRC="/etc/hackpad/src"
 
 	echo "-->Editing configuration files"
         echo "sed start"
+        ls -a
 	sed 's:^export SCALA_HOME=".*$:export SCALA_HOME="/usr/share/java":' -i'' bin/exports.sh
 	sed 's:^export SCALA_LIBRARY_JAR=".*$:export SCALA_LIBRARY_JAR="$SCALA_HOME/scala-library.jar":' -i'' bin/exports.sh
 	sed 's:^export JAVA_HOME=".*$:export JAVA_HOME="/usr/share/java":' -i'' bin/exports.sh
